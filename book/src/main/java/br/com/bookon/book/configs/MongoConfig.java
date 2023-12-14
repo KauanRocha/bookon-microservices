529eb569a0388e8b0aa2b67fc5614d2735c86937
KauanRocha <kauanrocha.professional@gmail.com>
2023-12-14 09:42:38 -0300

    :wrench: fix: Corrige as configurações do docker.

--- book/src/main/java/br/com/bookon/book/configs/MongoConfig.java
+++ book/src/main/java/br/com/bookon/book/configs/MongoConfig.java
@@ -5,13 +5,10 @@ import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
