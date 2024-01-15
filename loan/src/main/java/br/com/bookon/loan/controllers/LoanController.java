797f53d160e890637b80bdb24d16d06f52c76111
Kauan Rodrigues Oliveira Rocha <kauan.rocha@yuzer.com.br>
2024-01-15 13:01:37 -0300

    Implements kafka producer

--- loan/src/main/java/br/com/bookon/loan/controllers/LoanController.java
+++ loan/src/main/java/br/com/bookon/loan/controllers/LoanController.java
@@ -1,17 +1,18 @@
 package br.com.bookon.loan.controllers;
 
 import br.com.bookon.loan.enums.LoanStatusEnum;
-import br.com.bookon.loan.models.Loan;
 import br.com.bookon.loan.payloads.request.LoanRequest;
 import br.com.bookon.loan.payloads.response.LoanResponse;
 import br.com.bookon.loan.services.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/loans")
@@ -19,9 +20,25 @@ public class LoanController {
 
     private final LoanService loanService;
 
+
+    private KafkaTemplate<String, LoanRequest> kafkaTemplate;
+
     @Autowired
     public LoanController(LoanService loanService) {
         this.loanService = loanService;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping("/propose/create/v2")
+    public ResponseEntity<LoanResponse> createProposev2(@RequestBody @Valid LoanRequest loanRequest, @RequestParam Integer borrowerId) {
+        kafkaTemplate.send("loan-topic", loanRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/propose/create")
+    public ResponseEntity<LoanResponse> createPropose(@RequestBody @Valid LoanRequest loanRequest, @RequestParam Integer borrowerId) {
+        var createdLoan = loanService.createPropose(loanRequest, borrowerId);
+        return new ResponseEntity<>(createdLoan, HttpStatus.CREATED);
     }
 
     @GetMapping("/me/propose/search")
@@ -31,12 +48,7 @@ public class LoanController {
             return new ResponseEntity<>(loans, HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(loans, HttpStatus.OK);
-    }
 
-    @PostMapping("/propose/create")
-    public ResponseEntity<LoanResponse> createPropose(@RequestBody @Valid LoanRequest loanRequest, @RequestParam Integer borrowerId) {
-        var createdLoan = loanService.createPropose(loanRequest, borrowerId);
-        return new ResponseEntity<>(createdLoan, HttpStatus.CREATED);
     }
 
     @GetMapping("propose/search")
---
+++ loan/src/main/java/br/com/bookon/loan/kafka/KafkaConfig.java
@@ -0,0 +1,35 @@
+package br.com.bookon.loan.kafka;
+
+import br.com.bookon.loan.payloads.request.LoanRequest;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KafkaConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers:localhost:9092}")
+    private String bootstrapAddress;
+
+    @Bean
+    public ProducerFactory<String, LoanRequest> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapAddress);
+        configProps.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        configProps.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+}
